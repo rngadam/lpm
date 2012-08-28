6c54360d10cc985396a40b9ded456e698a2ca69f
Ricky Ng-Adam <rngadam@lophilo.com>
2012-08-28 18:45:46 +0800

    moving packages

--- flat_tree.js
+++ flat_tree.js
@@ -28,8 +28,10 @@ var generate_candidates = function(dir, candidate_dir) {
 var read_package = function(file) {
 	try {
 		var parsed = JSON.parse(fs.readFileSync(file));
+		parsed = _.pick(parsed, ['name', 'dependencies', 'devDependencies', 'version']);
 		console.log("read " + file);
 		parsed.location = file;
+		parsed.depth = file.split('/').length
 		return parsed;
 	} catch(e) {
 		if(e.code == 'ENOENT')
@@ -48,17 +50,21 @@ var postfix = function(value, dir) {
 	return path.join(dir, value); 	
 };
 
+var dirname_eq = function(a, b) {
+	return path.dirname(a) === b;
+}
+
 var load_packages = function(store_package, dir) {
 	try {
 		_.chain(fs.readdirSync(dir))
-				.map(prepostfix.bind(this, dir, 'package.json'))
+				.map(prepostfix.bind(null, dir, 'package.json'))
 				.map(read_package)
-				.filter(function(v) {return v;})
+				.filter(_.identity)
 				.map(store_package)
 				.pluck('location')
-				.map(postfix.bind(this, 'node_modules'))
+				.map(postfix.bind(null, 'node_modules'))
 				.map(load_packages.bind(this, store_package))
-				.filter(function(v) {return v;});
+				.filter(_.identity);
 	} catch(e) {
 		if(e.code == 'ENOENT')
 			return null;	
@@ -66,20 +72,41 @@ var load_packages = function(store_package, dir) {
 	}
 }
 
+
+var move_package = function(dir, pkg_info) {
+	console.log('mv ' + path.dirname(pkg_info.location) + ' ' + dir + '-' + pkg_info.version);
+};
+
+// expecting: [{pkg_info},{pkg_info},{pkg_info}]
+var move_packages = function(dir, non_root) {
+	_.map(non_root, move_package.bind(null, dir));
+};
+
+var debug_object = function(object) {
+	console.log(object);
+	return object;
+}
 var packages = [];
 var name_version_info = {};
-var store_package = function(package_information) {
-	packages.push(package_information);
-	if(name_version_info[package_information.name] === undefined) {
-		name_version_info[package_information.name] = {};		
+var store_package = function(pkg_info) {
+	packages.push(pkg_info);
+	if(name_version_info[pkg_info.name] === undefined) {
+		name_version_info[pkg_info.name] = {};		
 	}
-	if(name_version_info[package_information.name][package_information.version] === undefined) {
-		name_version_info[package_information.name][package_information.version] = [];		
+	if(name_version_info[pkg_info.name][pkg_info.version] === undefined) {
+		name_version_info[pkg_info.name][pkg_info.version] = [];		
 	}
-	name_version_info[package_information.name][package_information.version].push(package_information);
-	return package_information;
+	name_version_info[pkg_info.name][pkg_info.version].push(pkg_info);
+	return pkg_info;
 };
 
 load_packages(store_package, dir);
 
+var non_root = _.chain(packages)
+	.reject(dirname_eq.bind(null, dir))
+	.groupBy('depth')
+	.sortBy(function(v) {1.0/v;})
+	.map(move_packages.bind(null, dir));
+
+console.log(non_root);
 console.log('finished!');
